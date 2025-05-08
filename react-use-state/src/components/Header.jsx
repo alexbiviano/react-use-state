048206fc5773c5250272db5df9f477d9c05a0dab
alexbiviano <bivianoalex63@gmail.com>
2025-05-08 15:30:35 +0200

    [upd] done header

--- react-use-state/src/components/Header.jsx
+++ react-use-state/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <header>
+                <h1>Learn Web development</h1>
+            </header>
+        </>
+    )
 }
 
 export default Header
